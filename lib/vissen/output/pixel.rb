6a1c9b49d92d9d42109b1af3470bfc7c7b8b28cb
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-04-12 12:53:52 +0200

    Improved the documentation

--- lib/vissen/output/pixel.rb
+++ lib/vissen/output/pixel.rb
@@ -2,16 +2,22 @@
 
 module Vissen
   module Output
-    # Pixel
+    # The `Pixel` object is used by the `PixelCloud` to store the colors of its
+    # points.
     #
     # TODO: How do we want the pixel to saturate? When written or when read?
     class Pixel < Color
+      # Reset the pixel color to black (0, 0, 0).
+      #
+      # @return [self]
       def clear!
         self.r = 0
         self.g = 0
         self.b = 0
+        self
       end
 
+      # @return [String] a string representation of the pixel.
       def inspect
         format '(%.1f, %.1f, %.1f)', r, g, b
       end
