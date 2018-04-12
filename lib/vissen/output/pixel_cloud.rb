fc2d8dfc3fdfef1fd57e3caa68c5fe97c8e3290f
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-04-12 11:29:28 +0200

    Improved the documentation

--- lib/vissen/output/pixel_cloud.rb
+++ lib/vissen/output/pixel_cloud.rb
@@ -2,22 +2,24 @@
 
 module Vissen
   module Output
-    # Pixel Cloud
-    #
-    #
+    # The pixel cloud is an implementation of the general `Cloud` module that
+    # places `Pixel` objects in each cloud point.
     class PixelCloud
       include Cloud
 
+      # @!method pixels
+      # @return [Pixel] the pixels in the cloud.
       alias pixels elements
 
+      # @param  context [Context] the context in which the pixel cloud exists.
       def initialize(context)
         super context, Pixel
         freeze
       end
 
-      # Copy!
-      #
       # Replaces the pixel values of this buffer with those of the given object.
+      #
+      # @param  other [PixelCloud] the pixel cloud to copy values from.
       def copy!(other)
         raise TypeError unless other.is_a? self.class
 
@@ -30,8 +32,6 @@ module Vissen
         end
       end
 
-      # Clear!
-      #
       # Zeros the rbg components of each pixel in the buffer.
       def clear!
         pixels.each(&:clear!)
